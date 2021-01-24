06d46560e07c8ebe24322307cb1dd23d4ba9dd6e
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2021-01-25 00:20:02 +0900

    Fix crash on some event taps

--- core/src/main/java/com/alamkanak/weekview/WeekView.kt
+++ core/src/main/java/com/alamkanak/weekview/WeekView.kt
@@ -1521,7 +1521,8 @@ class WeekView @JvmOverloads constructor(
                 candidates.isEmpty() -> null
                 // Two events hit. This is most likely because an all-day event was clicked, but a
                 // single event is rendered underneath it. We return the all-day event.
-                candidates.size == 2 -> candidates.first { it.event.isAllDay }.takeUnless { it.isHidden }
+                candidates.size == 2 && candidates.any { it.event.isAllDay } ->
+                    candidates.first { it.event.isAllDay }.takeUnless { it.isHidden }
                 else -> candidates.first().takeUnless { it.isHidden }
             }
         }
